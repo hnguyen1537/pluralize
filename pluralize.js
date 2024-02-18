56f2f6c08accf3e56fb05ccbf50ac79a559fa4a8
hnguyen1537 <hnguyen1537@student.cccd.edu>
2024-02-18 11:13:29 -0800

    update

--- pluralize.js
+++ pluralize.js
@@ -2,14 +2,19 @@
 // a noun and count.
 function pluralize(noun, count) {
   // TODO: Implement this function.
-  const pluralize = (noun,count)
+
+    // if count is equal to or less than 0 , return count and noun+s
+    if (count === 0 || count < 0) {
+      return `${count} ${noun}s`;
+    }
   
-  return `${count}${noun}`;
-}
-
-
-console.log(pluralize('cat', 5)); // return result 
-console.log(pluralize('dog', 1)); // return result 
+    // Use a simple rule for pluralization
+    return `${count} ${count === 1 ? noun : noun + 's'}`;
+  }
+  //checking console
+  console.log(pluralize('cat', 5));    // "5 cats"
+  console.log(pluralize('dog', 1));    // "1 dog"
+  console.log(pluralize('apple', 1));  // "1 apple"
 
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
